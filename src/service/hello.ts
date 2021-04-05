af09d7fd4cb9443acaae6a29c1f4eeb7a63aeb11
Felipe Patricio <alvespatriciofelipe@gmail.com>
2021-04-05 20:34:29 -0300

    remove api.ts

--- src/service/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiRequest, NextApiResponse } from "next";
-
-// type Data = any;
-
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  response.status(200).json({ name: "John Doe" });
-};
