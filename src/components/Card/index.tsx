e33b1d5cda626bf08fc81ea208f881763827ef5f
Felipe Patricio <alvespatriciofelipe@gmail.com>
2021-04-05 20:29:20 -0300

    any

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,9 @@
 import styles from './styles/Card.module.css'
 
-const Card = ({data}) => {
+const Card = ({data}: any) => {
   return (
     <ul className={styles.container}>
-      {data.map(({name, id}) => (
+      {data.map(({name, id}: any) => (
         <li key={id} className={styles.card}>
           <a href={`/launches/${id}`} className={styles.anchor}>{name}</a>
         </li>
