66513fd030cde03cb8da7e382dcce2cc1b582cc6
Felipe Patricio <alvespatriciofelipe@gmail.com>
2021-04-21 00:57:41 -0300

    react memo

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,36 +4,36 @@ import {
   CardContent,
   CardHeader,
   Typography,
-  Card as MuiCard, Button
+  Card as MuiCard,
+  Button,
 } from "@material-ui/core";
 import { format } from "date-fns";
 import styles from "./styles/Card.module.css";
 
-const Card = ({ data }: any) => {
+const Card = ({ data }: any) => (
+  <ul className={styles.container}>
+    {data.map(({ name, id, details, date_utc }: any) => (
+      <>
+        <MuiCard key={id} className={styles.card}>
+          <CardHeader
+            action={
+              <Button href={`launches/${id}`} color="primary">
+                Detalhes
+              </Button>
+            }
+            title={name}
+            subheader={format(new Date(date_utc), "d/M/yyyy")}
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {details}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing />
+        </MuiCard>
+      </>
+    ))}
+  </ul>
+);
 
-  return (
-    <ul className={styles.container}>
-      {data.map(({ name, id, details, date_utc }: any) => (
-        <>
-          <MuiCard key={id} className={styles.card}>
-            <CardHeader
-              action={
-                <Button href={`launches/${id}`} color="primary">Detalhes</Button>
-              }
-              title={name}
-              subheader={format(new Date(date_utc), "d/M/yyyy")}
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {details}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing />
-          </MuiCard>
-        </>
-      ))}
-    </ul>
-  );
-};
-
-export default Card;
+export default React.memo(Card);
