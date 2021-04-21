30d694e89e4b5861f7964afe4c96e026ad96bd2e
Felipe Patricio <alvespatriciofelipe@gmail.com>
2021-04-21 00:39:09 -0300

    remove card-image

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,7 +3,6 @@ import {
   CardActions,
   CardContent,
   CardHeader,
-  CardMedia,
   Typography,
   Card as MuiCard, Button
 } from "@material-ui/core";
@@ -19,14 +18,10 @@ const Card = ({ data }: any) => {
           <MuiCard key={id} className={styles.card}>
             <CardHeader
               action={
-                <Button href={`${id}`} color="primary">Detalhes</Button>
+                <Button href={`launches/${id}`} color="primary">Detalhes</Button>
               }
               title={name}
               subheader={format(new Date(date_utc), "d/M/yyyy")}
-            />
-            <CardMedia
-              image="/static/images/cards/paella.jpg"
-              title="Paella dish"
             />
             <CardContent>
               <Typography variant="body2" color="textSecondary" component="p">
