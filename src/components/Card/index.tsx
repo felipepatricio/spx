bb285d8ff16a8f5bfc5d49ce55af47645979675d
Felipe Patricio <alvespatriciofelipe@gmail.com>
2021-04-20 23:48:55 -0300

    remove svg

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import {
-  Avatar,
   CardActions,
   CardContent,
   CardHeader,
   CardMedia,
-  IconButton,
   Typography,
-  Card as MuiCard,
+  Card as MuiCard, Button
 } from "@material-ui/core";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { format } from "date-fns";
 import styles from "./styles/Card.module.css";
 
@@ -21,11 +18,8 @@ const Card = ({ data }: any) => {
         <>
           <MuiCard key={id} className={styles.card}>
             <CardHeader
-              avatar={<Avatar aria-label="recipe">R</Avatar>}
               action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
+                <Button href={`${id}`} color="primary">Detalhes</Button>
               }
               title={name}
               subheader={format(new Date(date_utc), "d/M/yyyy")}
